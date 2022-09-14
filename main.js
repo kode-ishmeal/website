bdfe5e5b09d0741f8155740b45d65e0841d9bfd6
kode-ishmeal <110170028+kode-ishmeal@users.noreply.github.com>
2022-09-14 15:00:01 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+const cityName = document.querySelector("#city-name");
+const temperature = document.querySelector(".temperature");
+const windSpeed = document.querySelector(".wind");
+const skyCondition = document.querySelector(".sky-condition");
+const form = document.querySelector("#form-container");
+
+// Fetching the weather data from the API
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let city = form.cityname.value;
+  cityName.textContent = city;
+  const API_KEY = "3045dd712ffe6e702e3245525ac7fa38";
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      temperature.innerHTML += `<p>${Math.round(
+        data.main.temp - 273
+      )}&deg;C</p>`;
+      windSpeed.innerHTML += `<p>${data.wind.speed} m/s</p>`;
+      skyCondition.innerHTML += `<p>${data.weather[0].description}</p>`;
+    })
+    .catch((err) => {
+      alert("Please enter a valid city name");
+    });
+});
